7c6488b7e198574ca58af236603c7d4886e07e80
ahalshaikh <ahalshaikh@mail.csuchico.edu>
2018-02-28 17:38:08 -0800

    repeat the games as told

--- main.cpp
+++ main.cpp
@@ -5,7 +5,33 @@ using namespace std;
 
 int main()
 {
-  SecretDoor game;
-    
-  return 0;
+        SecretDoor game;
+
+        int gamesRepeat = 0;
+
+        int gamesWon = 0;
+
+        char input;
+
+        cout<<"How many times would you like to play?"<<endl;
+        cin>>gamesRepeat;
+
+        for(int i = 0; i < gamesRepeat; i++)
+        {
+                game.newGame();
+
+                game.guessDoorC();
+                game.guessDoorC();
+
+
+                if( game.isWinner() == true)
+                {
+                        gamesWon++;
+                }
+        }
+
+        cout<<"You won "<<gamesWon<<" times."<<endl;
+
+
+        return 0;
 }
